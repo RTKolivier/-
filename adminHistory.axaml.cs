799156c927ddf4c17c9f455eb796ec1092868a4c
Dmitriy_Serov <dima.bessarab@list.ru>
2025-06-18 13:45:04 +0300

    lol

---
+++ adminHistory.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace курчас;
+
+public partial class adminHistory : Window
+{
+    public adminHistory()
+    {
+        InitializeComponent();
+    }
+}
--- adminReg.axaml.cs
+++ adminReg.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.ComponentModel.Design;
 
 namespace курчас;
 
@@ -13,8 +14,19 @@ public partial class adminReg : Window
     }
     private void Admin_Permit_OnClick(object? sender, RoutedEventArgs e)
     {
-        //adapter adapterWindow = new adapter();
-        //adapterWindow.Show();
-        //Close();
+        const string adminLogin = "Артур";
+        const string adminPassword = "ArtMastex";
+        if (adminLogin == LoginAdmin.Text &&
+            adminPassword == PasswordAdmin.Text)
+        {
+            adminHistory adminHistory = new adminHistory();
+            adminHistory.Show();
+            Close();
+        }
+        else
+        {
+            Error.Text = "ошибка ввода";
+        }
     }
+    
 }
