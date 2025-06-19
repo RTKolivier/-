759e6e79169527bf317867e424aeaff4c19f262e
Dmitriy_Serov <dima.bessarab@list.ru>
2025-06-19 14:19:56 +0300

    lol

--- clientReg.axaml.cs
+++ clientReg.axaml.cs
@@ -2,17 +2,21 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.Linq;
+using курчас.Models;
 
 namespace курчас;
 
 public partial class clientReg : Window
 {
+    public Client lastclient = DbHelper.context.Clients.OrderBy(x => x.ClientsId).Last();
     public clientReg()
     {
         InitializeComponent();
     }
     private void Client_Permit_OnClick(object? sender, RoutedEventArgs e)
     {
+        AddClient();
         clientAdapterSec clientAdapterSec = new clientAdapterSec();
         clientAdapterSec.Show();
         Close();
@@ -22,5 +26,23 @@ public partial class clientReg : Window
         clientAdapter clientAdapter = new clientAdapter();
         clientAdapter.Show();
         Close();
+    }
+    public void AddClient()
+    {
+        Client newClients = new Client
+        {
+            ClientsId = lastclient.ClientsId + 1,
+            ClientsNick = NameClient.Text,
+            ClientsUsername = UserClient.Text,
+            ClientsFio = FIOClient.Text,
+            ClientsPhone = PhoneClient.Text,
+            ClientsCity = CityClient.Text,
+            ClientsOpp = OPPClient.Text,
+            ClientsAddress = AddressClient.Text,
+            ClientsPassword = PasswordClient.Text
+        };
+        DbHelper.context.Clients.Add(newClients);
+        DbHelper.context.SaveChanges();
+
     }
 }
